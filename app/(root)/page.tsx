c0573d104c2af5154209036cb05093d7ea2f7978
Mahmudul Hasan Anabil <hasanabilna@gmail.com>
2024-04-24 19:59:00 +0600

    initial setup done

---
+++ app/(root)/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <>
+      <main>
+        <h1 className="text-4xl">Evently</h1>
+        <Button variant="destructive" className="px-10">
+          Remove
+        </Button>
+      </main>
+    </>
+  );
+}
